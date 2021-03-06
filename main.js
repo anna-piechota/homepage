b5f81c3e80294148d91c2931aaa6d86817689271
AnnaPiechota <anna_piechota@wp.pl>
2021-03-06 16:35:59 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const firstName = 'Ania';
+const age = 52;
+console.log(firstName);
+console.log(age);
+console.log(`Nazywam się ${firstName} i mam ${age} lata`);
+alert('Witam w Pi Engineer');
+console.log('Witam')
